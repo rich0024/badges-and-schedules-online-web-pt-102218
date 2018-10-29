d1ec16fb9be5c5c3f85de6a9cfccf8baf382bad0
Richard Yance <yance0022@gmail.com>
2018-10-29 01:08:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(phrase)
+  if phrase = "attendees"
+    puts "badges"
+  if phrase = "[Johnny]"
+    puts "[""Hello, my name is Johnny."]"
+  end
